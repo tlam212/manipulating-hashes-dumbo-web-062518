2f5671d523d963a1b3042cc9c30ad8d62545de33
Toni Lamont <tlam212@gmail.com>
2018-06-21 20:48:34 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,6 +17,7 @@ contacts.each do |person, data|
     
     # if attribute == :favorite_icecream_flavors
     #   value.each do |flavor|
+    :favorite_icecream_flavors.deletif{
     #     delete_if 
 contacts
 end
